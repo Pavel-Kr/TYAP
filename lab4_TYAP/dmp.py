d539a472cf1f0d2d2e87e3e6dd315b9f26e09f8f
Pavel-Kr <57057057+Pavel-Kr@users.noreply.github.com>
2023-12-05 13:31:53 +0300

    Add files via upload

---
+++ lab4_TYAP/dmp.py
@@ -0,0 +1,247 @@
+from iteration_utilities import duplicates
+def remove_curly_brackets(string):
+    if string[0] != '{' or string[-1] != '}':
+        print("Syntax error: no curly brackets")
+        return
+    return string[1:-1]
+
+
+def split_set(string):
+    no_brackets = remove_curly_brackets(string)
+    if no_brackets is not None:
+        return no_brackets.split(",")
+    return None
+
+
+def parse_description(description):
+    no_spaces = str.join("", description.split())
+    desc = str.split(no_spaces, ";")
+    if len(desc) != 7:
+        return None, "Ошибка описания автомата: не хватает деталей описания"
+    res = {}
+
+    states = split_set(desc[0])
+    if states is None:
+        return None, "Синтаксическая ошибка в описании состояний автомата"
+    dups = list(duplicates(states))
+    if len(dups) != 0:
+        return None, f"Состояния {dups} встречаются больше одного раза"
+    res["states"] = states
+
+    alphabet_in = split_set(desc[1])
+    if alphabet_in is None:
+        return None, "Синтаксическая ошибка в описании входного алфавита"
+    dups = list(duplicates(alphabet_in))
+    if len(dups) != 0:
+        return None, f"Символы {dups} встречаются во входном алфавите больше одного раза"
+    for a in alphabet_in:
+        if len(a) != 1:
+            return None, f"Неправильный символ входного алфавита {a}, длина символа должна быть равна 1"
+    res["alphabet in"] = alphabet_in
+
+    stack_alphabet = split_set(desc[2])
+    if stack_alphabet is None:
+        return None, "Синтаксическая ошибка в описании алфавита стека"
+    dups = list(duplicates(stack_alphabet))
+    if len(dups) != 0:
+        return None, f"Символы {dups} встречаются в алфавите стека больше одного раза"
+    for a in stack_alphabet:
+        if len(a) != 1:
+            return None, f"Неправильный символ алфавита стека {a}, длина символа должна быть равна 1"
+    res["stack alphabet"] = stack_alphabet
+
+    alphabet_out = split_set(desc[3])
+    if alphabet_out is None:
+        return None, "Синтаксическая ошибка в описании выходного алфавита"
+    dups = list(duplicates(alphabet_out))
+    if len(dups) != 0:
+        return None, f"Символы {dups} встречаются в выходном алфавите больше одного раза"
+    for a in alphabet_out:
+        if len(a) != 1:
+            return None, f"Неправильный символ выходного алфавита {a}, длина символа должна быть равна 1"
+    res["alphabet out"] = alphabet_out
+
+    start_state = desc[4]
+    if start_state not in states:
+        return None, f"Стартовое состояние {start_state} отсутствует в списке состояний"
+    res["start state"] = start_state
+
+    start_stack = desc[5]
+    if start_stack not in stack_alphabet:
+        return None, f"Стартовый символ стека {start_stack} отсутствует в алфавите стека"
+    res["start stack"] = start_stack
+
+    final_states = split_set(desc[6])
+    if final_states is None:
+        return None, "Синтаксическая ошибка в описании конечных состояний автомата"
+    dups = list(duplicates(final_states))
+    if len(dups) != 0:
+        return None, f"Конечные состояния {dups} встречаются больше одного раза"
+    for s in final_states:
+        if s not in states:
+            return None, f"Конечное состояние {s} отсутствует в списке состояний"
+    res["final states"] = final_states
+    return res, ""
+
+
+def remove_brackets(string):
+    if string[0] != '(' or string[-1] != ')':
+        print("Syntax error: no brackets")
+        return
+    return string[1:-1]
+
+
+def parse_transition(transition):
+    no_spaces = str.join("", transition.split())
+    transition_spilt = str.split(no_spaces, "=")
+    if len(transition_spilt) != 2:
+        return None
+
+    state = remove_brackets(transition_spilt[0])
+    action = remove_brackets(transition_spilt[1])
+    state_split = str.split(state, ",")
+    if len(state_split) != 3:
+        return None
+    action_split = str.split(action, ",")
+    if len(action_split) != 3:
+        return None
+
+    for s in state_split:
+        if s == "":
+            return None
+
+    for s in action_split:
+        if s == "":
+            return None
+
+    if state_split[1] == "E":
+        state_split[1] = ""
+
+    if state_split[2] == "E":
+        state_split[2] = ""
+
+    if action_split[1] == "E":
+        action_split[1] = ""
+
+    if action_split[2] == "E":
+        action_split[2] = ""
+
+    return state_split, action_split
+
+
+class DMP:
+    dmp = None
+    current_state = ""
+    stack = ""
+    input_string = ""
+    output_string = ""
+
+    def load_dmp_from_file(self, file_path):
+        with open(file_path) as f:
+            description = f.readline()
+            m = parse_description(description)
+            if m[0] is None:
+                return m[1]
+            self.dmp = m[0]
+            table = [s for s in list(f) if s != '\n']
+            states = self.dmp["states"]
+            trans_dict = {}
+            for i in range(len(table)):
+                if table[i][-1] == '\n':
+                    table[i] = table[i][:-1]
+                transitions = parse_transition(table[i])
+                if transitions is None:
+                    return f"Синтаксическая ошибка в {i + 1}-м правиле: ошибка чтения перехода"
+                alphabet_in = self.dmp["alphabet in"]
+                stack_alphabet = self.dmp["stack alphabet"]
+                alphabet_out = self.dmp["alphabet out"]
+                d = {}
+                state = transitions[0]
+                action = transitions[1]
+                if state[0] not in states:
+                    return f"Синтаксическая ошибка в {i + 1}-м правиле: состояние {state[0]} отсутствует в списке состояний"
+                if state[1] != "" and state[1] not in alphabet_in:
+                    return f"Синтаксическая ошибка в {i + 1}-м правиле: символ {state[1]} отсутствует в входном алфавите"
+                if state[2] != "" and state[2] not in stack_alphabet:
+                    return f"Синтаксическая ошибка в {i + 1}-м правиле: символ {state[2]} отсутствует в алфавите стека"
+                if action[0] not in states:
+                    return f"Синтаксическая ошибка в {i + 1}-м правиле: состояние {action[0]} отсутствует в списке состояний"
+                for sym in action[1]:
+                    if sym not in stack_alphabet:
+                        return f"Синтаксическая ошибка в {i + 1}-м правиле: символ {sym} отсутствует в алфавите стека"
+                for sym in action[2]:
+                    if sym not in alphabet_out:
+                        return f"Синтаксическая ошибка в {i + 1}-м правиле: символ {sym} отсутствует в выходном алфавите"
+                state_tr = trans_dict.get(state[0])
+                if state_tr is None:
+                    d1 = {state[2]: action}#(action[0], action[1])}
+                    d[state[1]] = d1
+                    trans_dict[state[0]] = d
+                    continue
+
+                str_sym_tr = state_tr.get(state[1])
+                if str_sym_tr is None:
+                    d[state[2]] = action #(action[0], action[1])
+                    state_tr[state[1]] = d
+                    continue
+
+                stack_sym_tr = str_sym_tr.get(state[2])
+                if stack_sym_tr is not None:
+                    return f"Синтаксическая ошибка в {i + 1}-м правиле: переход по состоянию {state} уже существует"
+
+                str_sym_tr[state[2]] = action #(action[0], action[1])
+            self.dmp["transitions"] = trans_dict
+            print(self.dmp)
+            return ""
+
+    def print_state(self):
+        ret_input = self.input_string
+        if ret_input == "":
+            ret_input = "E"
+        ret_stack = self.stack
+        if ret_stack == "":
+            ret_stack = "E"
+        ret_output = self.output_string
+        if ret_output == "":
+            ret_output = "E"
+        return f"({self.current_state}, {ret_input}, {ret_stack}, {ret_output})"
+
+    def init_check(self, string):
+        if self.dmp is None:
+            return False, "Конечный автомат не инициализирован"
+        self.input_string = string
+        self.current_state = self.dmp["start state"]
+        self.stack = self.dmp["start stack"]
+        self.output_string = ""
+        return True, self.print_state()
+
+    def check_step(self):
+        if self.input_string == "" and self.stack == "":
+            if self.current_state not in self.dmp["final states"]:
+                return False, "\nЦепочка не распознана: автомат не в конечном состоянии"
+            return False, "\nЦепочка распознана"
+
+        if self.stack == "":
+            return False, "\nЦепочка не распознана: стек пуст, но на ленте еще остались символы"
+
+        sym = ""
+        if self.input_string != "":
+            sym = self.input_string[0]
+            if sym not in self.dmp["alphabet in"]:
+                return False, f"\nЦепочка не распознана: символ {sym} отсутствует в входном алфавите"
+
+        sym_tr = self.dmp["transitions"][self.current_state].get(sym)
+        if sym_tr is None:
+            return False, f"\nЦепочка не распознана: нет перехода из состояния {self.current_state} по символу {sym}"
+
+        stack_top = self.stack[0]
+        self.stack = self.stack[1:]
+        action = sym_tr.get(stack_top)
+        if action is None:
+            return False, f"\nЦепочка не распознана: нет перехода из состояния ({self.current_state}, {sym}, {stack_top})"
+
+        self.current_state = action[0]
+        self.input_string = self.input_string[1:]
+        self.stack = action[1] + self.stack
+        self.output_string = self.output_string + action[2]
+        return True, self.print_state()
---
+++ lab4_TYAP/main.py
@@ -0,0 +1,65 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, \
+    QHBoxLayout, QWidget, QListWidget, QSpinBox, QLineEdit, QLabel, QPlainTextEdit, QFileDialog
+from dmp import DMP
+
+
+class MainWindow(QMainWindow):
+    dmp = DMP()
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("4 лаба ТЯП")
+        v_layout = QVBoxLayout()
+        self.string_edit = QLineEdit()
+        self.check_field = QPlainTextEdit()
+        self.check_field.setReadOnly(True)
+
+        check_button = QPushButton("Проверить цепочку")
+        check_button.clicked.connect(self.on_check_button_click)
+
+        load_button = QPushButton("Загрузить автомат")
+        load_button.clicked.connect(self.on_load_button_clicked)
+
+        v_layout.addWidget(self.string_edit)
+        v_layout.addWidget(self.check_field)
+        v_layout.addWidget(check_button)
+        v_layout.addWidget(load_button)
+
+        container = QWidget()
+        container.setLayout(v_layout)
+        self.setCentralWidget(container)
+
+    def on_load_button_clicked(self):
+        self.check_field.clear()
+        path = QFileDialog.getOpenFileName()[0]
+        if path != "":
+            error_msg = self.dmp.load_dmp_from_file(path)
+            if error_msg != "":
+                self.check_field.setPlainText(error_msg)
+
+    def on_check_button_click(self):
+        _string = self.string_edit.text()
+        progress = ""
+        start_config = self.dmp.init_check(_string)
+        if start_config[0] is False:
+            self.check_field.setPlainText(start_config[1])
+            return
+        progress += start_config[1]
+        self.check_field.setPlainText(progress)
+        step = self.dmp.check_step()
+        while step[0] is True:
+            progress += " |— "
+            progress += step[1]
+            self.check_field.setPlainText(progress)
+            step = self.dmp.check_step()
+        if step[1] != "":
+            progress += step[1]
+            self.check_field.setPlainText(progress)
+
+
+app = QApplication([])
+window = MainWindow()
+window.show()
+
+app.exec()
